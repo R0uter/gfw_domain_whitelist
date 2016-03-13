b7d1f7ba41b72e36e55674c3b32aca440b4ed232
R0uter <lomyr.jo@gmail.com>
2016-03-14 00:07:12 +0800

    update

--- lists/custom.py
+++ lists/custom.py
@@ -3,45 +3,6 @@
 
 def getlist():
     liststr = """
-clouddn.com
-zhihuishu.com
-cnswift.org
-qnssl.com
-233.wiki
-qingcdn.com
-logcg.com
-douyutv.com
-jiyifa.com
-bennythink.com
-linuxeden.com
-cn163.net
-weiphone.net
-fengbuy.com
-smartisan.com
-cloudxns.net
-myserverhosts.com
-softaculous.com
-0773188.com
-maorui.org
-coding.net
-u17i.com
-runoob.com
-10086.com
-aubdas.com
-videocc.net
-crashlytics.com
-rrmj.tv
-flypy.com
-ele.me
-Tower.im
-ncar.cc
-ngacn.cc
-steampowered.com
-youlu.net
-chacuo.net
-le.com
-lesports.com
-ebtang.com
-baidupcs.com
+
 """
     return set(liststr.splitlines(False))
