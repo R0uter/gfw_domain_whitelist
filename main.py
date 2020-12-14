b892a5e098084f181fc0860050e8e914c3ab8709
Tommy <4850853+wx00@users.noreply.github.com>
2020-12-14 18:01:25 +0800

    added support for local rulefile; reformated by black; fix proxy example.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-import sys
 import re
 import urllib3
 import certifi
@@ -9,20 +8,47 @@ import codecs
 
 from argparse import ArgumentParser
 
+
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('-i', '--input', dest='input', default=os.path.join('data','whitelist.pac'),
-        help='path to gfwlist')
-    parser.add_argument('-o', '--output', dest='output', default='whitelist.pac',
-        help='path to output pac', metavar='PAC')
-    parser.add_argument('-p', '--proxy', dest='proxy', default='"SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080;"',
+    parser.add_argument(
+        '-i',
+        '--input',
+        dest='input',
+        default=os.path.join('data', 'whitelist.pac'),
+        help='path to gfwlist',
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        dest='output',
+        default='whitelist.pac',
+        help='path to output pac',
+        metavar='PAC',
+    )
+    parser.add_argument(
+        '-p',
+        '--proxy',
+        dest='proxy',
+        default='"SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080;"',
         help='the proxy parameter in the pac file, for example,\
-        "127.0.0.1:1080;"', metavar='SOCKS5')
+        "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080;"',
+        metavar='SOCKS5',
+    )
+    parser.add_argument(
+        '-l',
+        '--local-rules',
+        dest='rules',
+        help='use local rule file for whitelist rules, use dnsmasq .conf format, '
+        'or one domain per line surrounded by "/".',
+        metavar='RULE_FILE',
+    )
     return parser.parse_args()
 
-def writefile(input_file, proxy, output_file):
 
-    domains_content = final_list()
+def writefile(input_file, proxy, output_file, rulesfile=None):
+
+    domains_content = final_list(rulesfile)
     proxy_content = get_file_data(input_file)
     proxy_content = proxy_content.replace('__PROXY__', proxy)
     proxy_content = proxy_content.replace('__DOMAINS__', domains_content)
@@ -31,7 +57,21 @@ def writefile(input_file, proxy, output_file):
         file_obj.write(proxy_content)
 
 
-def get_list():
+def get_local_list(filename):
+    whitelist = []
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                l = re.findall(r'(?<==/).+?(?=/)', line)
+                if l:
+                    whitelist.append('"{}":1,'.format(l[0]))
+    except IOError:
+        print('Unable to open local rule file, exiting...')
+        exit(1)
+    return whitelist
+
+
+def get_online_list():
     print('Getting domain whitelist...')
     dnsmasq_china_list = 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
     whitelist = []
@@ -41,21 +81,22 @@ def get_list():
         f = codecs.open('whitelistCache', 'w', 'utf-8')
         f.write(content)
         f.close()
-
     except:
         print('Get list update failed,use cache to update instead.')
 
     try:
         f = codecs.open('whitelistCache', 'r', 'utf-8')
     except IOError:
-        print('Unable to get domain whitelist, exiting...', file=sys.stderr)
+        print('Unable to get domain whitelist, exiting...')
         exit(1)
-    for line in f.readlines():
+
+    for line in f:
         l = re.findall(r'(?<==/).+?(?=/)', line)
-        whitelist.append('"'+l[0]+'":1,')
+        whitelist.append('"' + l[0] + '":1,')
     f.close()
 
     return whitelist
+
 
 def getList(listUrl):
     http = urllib3.PoolManager(
@@ -67,9 +108,11 @@ def getList(listUrl):
     return data
 
 
-def final_list():
-
-    list_result = get_list()
+def final_list(rulesfile):
+    if rulesfile is not None:
+        list_result = get_local_list(rulesfile)
+    else:
+        list_result = get_online_list()
     content = '\n'.join(list_result)
     content = '{\n' + content + '\n"yourdomainhere.com":1\n}'
     print('All done!')
@@ -85,8 +128,13 @@ def get_file_data(filename):
 
 def main():
     args = parse_args()
-    writefile(args.input, '"' + args.proxy.strip('"') + '"', args.output)
+    writefile(
+        args.input,
+        '"' + args.proxy.strip('"') + '"',
+        args.output,
+        args.rules,
+    )
+
 
 if __name__ == '__main__':
     main()
-
